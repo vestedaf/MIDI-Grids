0970682e528e184169e57f5461cb425b2b538b57
Your Name <you@example.com>
2026-06-05 13:02:31 -0700

    fix: remove unsafe flag and hardcode hex file path in bake target

--- avrlib/makefile.mk
+++ avrlib/makefile.mk
@@ -297,10 +297,11 @@ fuses:
 bootstrap: bake
 
 bake:	$(FIRMWARE)
-		$(AVRDUDE) $(AVRDUDE_COM_OPTS) $(AVRDUDE_ISP_OPTS) -B 10 -e -u \
+		$(AVRDUDE) $(AVRDUDE_COM_OPTS) $(AVRDUDE_ISP_OPTS) -B 10 -e \
 			-U efuse:w:0x$(EFUSE):m \
 			-U hfuse:w:0x$(HFUSE):m \
 			-U lfuse:w:0x$(LFUSE):m \
 			-U lock:w:0x$(LOCK):m
 		$(AVRDUDE) $(AVRDUDE_COM_OPTS) $(AVRDUDE_ISP_OPTS) -B 1 \
-			-U flash:w:$(TARGET_HEX):i -U lock:w:0x$(LOCK):m
+			-U flash:w:build/midi-grids.hex:i -U lock:w:0x$(LOCK):m
+
